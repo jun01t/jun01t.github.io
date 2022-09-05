87cdd76beef46f6a0b867cccab84103d04f75e5f
junichi <tmdjnch0901@gmail.com>
2022-09-05 18:24:02 +0900

    Header完成

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,19 +4,21 @@ import ProfileImage from "../images/profile-image.png"
 export const Header = () => {
     return (
         <header className="main-cover" style={{backgroundImage:`url(${CoverImage})`}}>
+        {}
         <div className="overlay"></div>
         <div className="container">
             <div className="display-table">
                 <div className="display-table-contents">
-                    <div className="portfolio-thumb" style={{backgroundImage:`url(${ProfileImage})`}}></div>
+                    {}
+                    <div className="profile-thumb" style={{backgroundImage:`url(${ProfileImage})`}}></div>
                     <h1 className="title-text">Junichi Tomida</h1>
                     <h3 className="title-text">Backend Enginner</h3>
                     <ul className="social-icons">
                         <li className="icon-link">
-
+                        {}
                         </li>
                         <li className="icon-link">
-
+                        {}
                         </li>
                     </ul>
                 </div>
